bdb27a051a95224af9ac2ffb4ca968efaca30cd9
xinghewanli <2088451786@qq.com>
2022-01-19 21:31:54 +0200

    第1次练习

--- demo-module-1/src/com/xingchen/MainTest.java
+++ demo-module-1/src/com/xingchen/MainTest.java
@@ -4,13 +4,6 @@ public class MainTest {
     private String age;
     public static void main(String[] args){
         System.out.println("xingchen");
-        System.out.println("xingchen");
-        System.out.println("xingchen");
-        System.out.println("xingchen");
-        System.out.println("xingchen");
-        System.out.println("xingchen");
-        System.out.println("xingchen");
-        System.out.println("xingchen");
-        System.out.println("xingchen");
+        System.out.println("xinghewanli,zhengchengweijin");
     }
 }
