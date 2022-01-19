3fdb5c3f7814e6b6253839b8c7d75de1957c452f
xinghewanli <2088451786@qq.com>
2022-01-19 21:07:40 +0200

    尝试进行第11次更改

--- demo-module-1/src/com/xingchen/MainTest.java
+++ demo-module-1/src/com/xingchen/MainTest.java
@@ -1,8 +1,8 @@
 package com.xingchen;
 
 public class MainTest {
+    private String age;
     public static void main(String[] args){
-
         System.out.println("xingchen");
         System.out.println("xingchen");
         System.out.println("xingchen");
