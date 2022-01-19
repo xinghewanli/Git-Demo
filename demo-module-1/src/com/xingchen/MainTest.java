738ea83ef38782b9d84b895d3a2845d9f796f247
xinghewanli <2088451786@qq.com>
2022-01-19 21:04:16 +0200

    尝试进行第10次更改

--- demo-module-1/src/com/xingchen/MainTest.java
+++ demo-module-1/src/com/xingchen/MainTest.java
@@ -2,7 +2,7 @@ package com.xingchen;
 
 public class MainTest {
     public static void main(String[] args){
-        System.out.println("xingchen");
+
         System.out.println("xingchen");
         System.out.println("xingchen");
         System.out.println("xingchen");
