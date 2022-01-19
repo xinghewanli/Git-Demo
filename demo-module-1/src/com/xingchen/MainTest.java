e3c6149b28c0f6fa9f1ba31daecbb7bfe91fee38
xinghewanli <2088451786@qq.com>
2022-01-19 21:48:12 +0200

    第3次练习

--- demo-module-1/src/com/xingchen/MainTest.java
+++ demo-module-1/src/com/xingchen/MainTest.java
@@ -6,6 +6,7 @@ public class MainTest {
         System.out.println("xingchen");
         System.out.println("Wonderful");
         System.out.println("666");
+        System.out.println("666");
         System.out.println("xinghewanli,zhengchengweijin");
 
     }
