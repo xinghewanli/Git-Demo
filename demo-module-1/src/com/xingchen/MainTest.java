7022e95375c58c1aab2b4e41f94c6f683f056a4f
xinghewanli <2088451786@qq.com>
2022-01-19 19:01:16 +0200

    尝试进行第三次更改

--- demo-module-1/src/com/xingchen/MainTest.java
+++ demo-module-1/src/com/xingchen/MainTest.java
@@ -4,5 +4,6 @@ public class MainTest {
     public static void main(String[] args){
         System.out.println("xingchen");
         System.out.println("xingchen");
+        System.out.println("xingchen");
     }
 }
