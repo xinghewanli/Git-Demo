444dd5534531f7a3d61c40fcde1c17d1925b16d0
xinghewanli <2088451786@qq.com>
2022-01-19 20:12:24 +0200

    尝试进行第六次更改

--- demo-module-1/src/com/xingchen/MainTest.java
+++ demo-module-1/src/com/xingchen/MainTest.java
@@ -7,5 +7,6 @@ public class MainTest {
         System.out.println("xingchen");
         System.out.println("xingchen");
         System.out.println("xingchen");
+        System.out.println("xingchen");
     }
 }
