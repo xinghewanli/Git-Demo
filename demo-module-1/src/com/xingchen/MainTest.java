6ef20e242da15398179e027a23ab908a2e635b37
xinghewanli <2088451786@qq.com>
2022-01-19 20:22:33 +0200

    尝试进行第八次更改

--- demo-module-1/src/com/xingchen/MainTest.java
+++ demo-module-1/src/com/xingchen/MainTest.java
@@ -9,5 +9,6 @@ public class MainTest {
         System.out.println("xingchen");
         System.out.println("xingchen");
         System.out.println("xingchen");
+        System.out.println("xingchen");
     }
 }
