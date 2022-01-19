5a190cd4753058a2aff3fcc76e9b1d8e0658e072
xinghewanli <2088451786@qq.com>
2022-01-19 17:49:08 +0200

    尝试进行第三次更改

--- demo-module-1/src/com/xingchen/MainTest.java
+++ demo-module-1/src/com/xingchen/MainTest.java
@@ -3,5 +3,6 @@ package com.xingchen;
 public class MainTest {
     public static void main(String[] args){
         System.out.println("xingchen");
+        System.out.println("xingchen");
     }
 }
